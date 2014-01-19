f764e148ba5efef0a769bdfdb2e2880e9c650f16
remogatto <andrea.fazzi@alcacoop.it>
2014-01-19 12:05:17 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Shaders
+
+<tt>shaders</tt> is a [Go](http://golang.org) package for handling OpenGL shader resources.
+
+# Installation
+
+<pre>
+go install github.com/remogatto/shaders
+</pre>
+
+# LICENSE
+
+See [LICENSE](LICENSE).
+
---
+++ shaders.go
@@ -0,0 +1,79 @@
+package shaders
+
+import (
+	"log"
+
+	gl "github.com/remogatto/opengles2"
+)
+
+type VertexShader string
+type FragmentShader string
+
+func checkShaderCompileStatus(shader uint32) {
+	var stat int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &stat)
+	if stat == 0 {
+		var length int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &length)
+		infoLog := gl.GetShaderInfoLog(shader, gl.Sizei(length), nil)
+		if len(infoLog) > 0 {
+			log.Fatalf("Compile error in shader %d: \"%s\"\n", shader, infoLog[:len(infoLog)])
+		}
+	}
+}
+
+func checkProgramLinkStatus(pid uint32) {
+	var stat int32
+	gl.GetProgramiv(pid, gl.LINK_STATUS, &stat)
+	if stat == 0 {
+		var length int32
+		gl.GetProgramiv(pid, gl.INFO_LOG_LENGTH, &length)
+		infoLog := gl.GetProgramInfoLog(pid, gl.Sizei(length), nil)
+		if len(infoLog) > 0 {
+			log.Fatalf("Link error in program %d: \"%s\"\n", pid, infoLog[:len(infoLog)])
+		}
+	}
+}
+
+func compileShader(typeOfShader gl.Enum, source string) uint32 {
+	if shader := gl.CreateShader(typeOfShader); shader != 0 {
+		gl.ShaderSource(shader, 1, &source, nil)
+		gl.CompileShader(shader)
+		checkShaderCompileStatus(shader)
+		return shader
+	}
+	return 0
+}
+
+func (s VertexShader) Compile() uint32 {
+	shaderId := compileShader(gl.VERTEX_SHADER, (string)(s))
+	return shaderId
+}
+
+func (s FragmentShader) Compile() uint32 {
+	shaderId := compileShader(gl.FRAGMENT_SHADER, (string)(s))
+	return shaderId
+}
+
+type Program uint32
+
+func NewProgram(fsh, vsh uint32) Program {
+	pid := gl.CreateProgram()
+	gl.AttachShader(pid, fsh)
+	gl.AttachShader(pid, vsh)
+	gl.LinkProgram(pid)
+	checkProgramLinkStatus(pid)
+	return Program(pid)
+}
+
+func (p Program) Use() {
+	gl.UseProgram(uint32(p))
+}
+
+func (p Program) GetAttribute(name string) uint32 {
+	return gl.GetAttribLocation(uint32(p), name)
+}
+
+func (p Program) GetUniform(name string) uint32 {
+	return gl.GetUniformLocation(uint32(p), name)
+}
